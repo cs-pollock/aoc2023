e96a2a55910ba525b1a580bd7aac79120684e8ba
Christian S. Pollock <christian.pollock@nestle.com>
2023-12-07 12:00:12 +0100

    Solve 4.1

--- aoc2023/Day04/Day04.cs
+++ aoc2023/Day04/Day04.cs
@@ -1,9 +1,18 @@
+using System.Text.RegularExpressions;
+using aoc2023.UtilsNS;
+
 namespace aoc2023.Day04;    
 public class Day04
 {
+    public static int Solve_01(string input) {
+        return Utils.SplitByLines(input)
+            .Select(cardString => ComputeCardResult(ParseScratchCard(cardString)))
+            .Aggregate(0, (total, cardResult) => total + cardResult);
+    }
+
     public static Scratchcard ParseScratchCard(string input) {
         var splitted = input.Split(':');
-        int id = int.Parse(input.Split(':')[0].Trim().Split(null)[1]);
+        int id = int.Parse(Regex.Split(splitted[0].Trim(), @"\s+")[1]);
         string[] allNumbers = splitted[1].Split('|');
         int[] winningNumbers = GetNumbersFromString(allNumbers[0]);
         int[] myNumbers = GetNumbersFromString(allNumbers[1]);
@@ -11,8 +20,18 @@ public class Day04
         return new(id, winningNumbers, myNumbers);
 
         static int[] GetNumbersFromString(string numberString) {
-            return numberString.Replace("  ", " ").Trim().Split(null).Select((string number) => int.Parse(number.Trim())).ToArray();
+            return numberString.Replace("  ", " ").Trim().Split(null).Select(numberString => int.Parse(numberString.Trim())).ToArray();
         }
+    }
+
+    public static int ComputeCardResult(Scratchcard scratchcard) {
+        return scratchcard.Winning.Aggregate(0, (result, current) => {
+            if (!scratchcard.Mine.Contains(current)) {
+                return result;
+            }
+
+            return result == 0 ? 1 : result * 2;
+        });
     }
 }
 
--- aoc2023/Day04/Day04Test.cs
+++ aoc2023/Day04/Day04Test.cs
@@ -5,12 +5,18 @@ namespace aoc2023.Day04;
 public class Day04Test
 {
     [TestMethod]
-    public void Test() {
-        var result = Day04.ParseScratchCard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+    public void Test_parser() {
+        var result = Day04.ParseScratchCard("Card  1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
         result.Id.Should().Be(1);
 
         result.Winning.Should().Equal(41, 48, 83, 86, 17);
         result.Mine.Should().Equal(83, 86, 6, 31, 17, 9, 48, 53);
+    }
+
+    [TestMethod]
+    public void Resolve_1() {
+        Day04.Solve_01(Day04Data.SimpleInput_01).Should().Be(13);
+        Day04.Solve_01(Day04Data.FullInput_01).Should().Be(26218);
     }
 
 }
