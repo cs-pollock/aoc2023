904b426b51751d77901abebf62bd26334df5ccfd
Christian S. Pollock <christian.pollock@nestle.com>
2023-12-07 11:26:36 +0100

    Add ParseScratchCard

--- aoc2023/Day04/Day04.cs
+++ aoc2023/Day04/Day04.cs
@@ -1,5 +1,19 @@
 namespace aoc2023.Day04;    
 public class Day04
 {
+    public static Scratchcard ParseScratchCard(string input) {
+        var splitted = input.Split(':');
+        int id = int.Parse(input.Split(':')[0].Trim().Split(null)[1]);
+        string[] allNumbers = splitted[1].Split('|');
+        int[] winningNumbers = GetNumbersFromString(allNumbers[0]);
+        int[] myNumbers = GetNumbersFromString(allNumbers[1]);
 
+        return new(id, winningNumbers, myNumbers);
+
+        static int[] GetNumbersFromString(string numberString) {
+            return numberString.Replace("  ", " ").Trim().Split(null).Select((string number) => int.Parse(number.Trim())).ToArray();
+        }
+    }
 }
+
+public record Scratchcard(int Id, int[] Winning, int[] Mine);
--- aoc2023/Day04/Day04Test.cs
+++ aoc2023/Day04/Day04Test.cs
@@ -1,10 +1,16 @@
+using FluentAssertions;
+
 namespace aoc2023.Day04;
 [TestClass]
 public class Day04Test
 {
     [TestMethod]
     public void Test() {
-        
+        var result = Day04.ParseScratchCard("Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53");
+        result.Id.Should().Be(1);
+
+        result.Winning.Should().Equal(41, 48, 83, 86, 17);
+        result.Mine.Should().Equal(83, 86, 6, 31, 17, 9, 48, 53);
     }
 
 }
